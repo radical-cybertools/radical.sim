67992b398f5d9031d29ae87df36aeca7f8bb436c
Mark Santcroos <mas781@scarletmail.rutgers.edu>
2014-10-16 11:20:41 +0200

    Bit nicer version construction.

--- src/radical/sim/version.py
+++ src/radical/sim/version.py
@@ -3,9 +3,7 @@ __license__   = "MIT"
 
 import os
 
-dirname = os.path.dirname(os.path.abspath(__file__))
+prefix = os.path.dirname(os.path.abspath(__file__))
 
-version = open(
-    os.path.join(dirname, "VERSION"), 'r').read().strip()
-version_detail = open(
-    os.path.join(dirname, "VERSION.git"), 'r').read().strip()
+version = open(os.path.join(prefix, "VERSION"), 'r').read().strip()
+version_detail = open(os.path.join(prefix, "VERSION.git"), 'r').read().strip()
