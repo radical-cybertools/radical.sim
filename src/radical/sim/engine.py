48415444eb774497224b2ab4683e70d853a2b55e
Mark Santcroos <mas781@scarletmail.rutgers.edu>
2014-10-17 16:33:34 +0200

    Engine to knot it all together.

---
+++ src/radical/sim/engine.py
@@ -0,0 +1,40 @@
+from simpy import Environment
+from compute_unit import ComputeUnit
+from compute_pilot import ComputePilot
+from dci import DCI
+from scheduler import Scheduler
+from logger import simlog
+from version import version, version_detail
+
+def run():
+
+    simlog.info('radical.sim version: %s (%s)' % (version, version_detail))
+
+    env = Environment()
+
+    stampede = DCI(env, "stampede", 1024)
+    archer = DCI(env, "archer", 2048)
+    sched = Scheduler(env, "BACK_FILLING")
+    pilot1 = ComputePilot(env, stampede, cores=3)
+    pilot2 = ComputePilot(env, archer, cores=4)
+
+    sched.add_pilot(pilot1)
+    sched.add_pilot(pilot2)
+
+    cu1 = ComputeUnit(env, cores=1)
+    sched.submit_cu(cu1)
+
+    cu2 = ComputeUnit(env, cores=2)
+    sched.submit_cu(cu2)
+
+    cu3 = ComputeUnit(env, cores=3)
+    sched.submit_cu(cu3)
+
+    cu4 = ComputeUnit(env, cores=4)
+    sched.submit_cu(cu4)
+
+    env.run(until=1000)
+
+
+if __name__ == '__main__':
+    run()
