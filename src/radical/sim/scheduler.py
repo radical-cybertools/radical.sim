55dc3a9bb4760c962800129a8e226663d7b8fc6e
Mark Santcroos <mas781@scarletmail.rutgers.edu>
2014-10-23 01:41:27 +0200

    Snapshot of scheduler for minimum simulation time based on timeouts.

--- src/radical/sim/scheduler.py
+++ src/radical/sim/scheduler.py
@@ -3,6 +3,8 @@ from collections import deque
 from states import ACTIVE, STATE_X
 from logger import simlog, INFO, DEBUG, WARNING
 
+SHORT=1
+
 class Scheduler(object):
     """BackFilling Scheduler"""
 
@@ -10,7 +12,7 @@ class Scheduler(object):
         self.env = env
         self.name = name
         self.pilots = []
-        self.new_cus = deque() # efficient operation on the left of the queue
+        self.new_cus = [] # Want to iterate over it
         self.active_cus = [] # Need a list as an argument to AnyOf
 
         simlog(INFO, "Intitializing scheduler %s." % self.name, self.env)
@@ -22,52 +24,52 @@ class Scheduler(object):
         simlog(INFO, "Scheduler %s starting." % self.name, self.env)
 
         while True:
-            # TODO: check for running pilots
-
             for pilot in self.pilots:
-
                 if pilot.state != ACTIVE:
-                    #self.env.timeout(100)
+                    # Don't try to run on inactive pilots
+                    yield self.env.timeout(SHORT) # Need this yield
                     continue
 
-                try:
-                    cu = self.new_cus.popleft()
+                # if not pilot.slots.items:
+                #     simlog(WARNING, "Pilot %d has no free slots, skipping this iteration." % pilot.id, self.env)
+                #     yield self.env.timeout(SHORT)
+                #     continue
 
-                    simlog(DEBUG, "Trying to schedule CU %d onto Pilot %d ..." % (
-                        cu.id, pilot.id), self.env)
+                activated_cus = []
+                for cu in self.new_cus:
+                    simlog(DEBUG, "Trying to schedule CU %d onto Pilot %d ..." % (cu.id, pilot.id), self.env)
                     if cu.cores <= len(pilot.slots.items):
-
+                        simlog(INFO, "Found pilot %d to schedule CU %d onto." % (pilot.id, cu.id), self.env)
+                        cu.pilot = pilot
+                        cu.stats['pilot'] = pilot.id
                         cu.slots = yield pilot.get(cu.cores)
                         cu.stats['slots'] = cu.slots
 
-                        simlog(INFO, "Found pilot %d to schedule CU %d onto." % (
-                            pilot.id, cu.id), self.env)
-
-                        cu.pilot = pilot
-                        cu.stats['pilot'] = pilot.id
-
+                        activated_cus.append(cu)
                         self.active_cus.append(self.env.process(cu.run()))
 
-                        self.env.cu_queue_history.append(
-                            {'time': self.env.now, 'length': len(self.new_cus)})
+                    elif cu.cores <= pilot.slots.capacity:
+                        simlog(WARNING, "Pilot %d has not enough free slots to schedule CU %d." % (pilot.id, cu.id), self.env)
                     else:
-                        simlog(WARNING, "Pilot %d has no capacity to schedule CU %d." % (
-                            pilot.id, cu.id), self.env)
-                        # Put it back in the queue
-                        self.new_cus.appendleft(cu)
+                        simlog(WARNING, "Pilot %d has not enough capacity to (ever) schedule CU %d." % (pilot.id, cu.id), self.env)
 
-                except IndexError:
-                    simlog(DEBUG, "No new CUs to process ...", self.env)
-                    yield self.env.timeout(100)
+                self.new_cus[:] = [cu for cu in self.new_cus if cu not in activated_cus]
+                self.env.cu_queue_history.append({'time': self.env.now, 'length': len(self.new_cus)})
 
-            finished = yield AnyOf(self.env, self.active_cus)
-            while finished:
-                (fin_proc, fin_cu) = finished.popitem()
-                self.active_cus.remove(fin_proc)
+            # Check for finished after looped over all pilots
+            #finished = yield AnyOf(self.env, self.active_cus)
+            #finished = yield AnyOf(self.env, self.active_cus+[self.env.timeout(2, 'timeout')])
+            #while finished:
+            finished = [cu for cu in self.active_cus if cu.processed]
+            for f in finished:
+                #(fin_proc, fin_cu) = finished.popitem()
+                fin_cu = yield f
+                #if fin_proc in self.active_cus:
+                self.active_cus.remove(f)
                 fin_cu.pilot.put(fin_cu.slots)
 
-            yield self.env.timeout(100)
-
+        # This yield is good!
+        yield self.env.timeout(SHORT)
 
     def submit_cu(self, cu):
         self.new_cus.append(cu)
